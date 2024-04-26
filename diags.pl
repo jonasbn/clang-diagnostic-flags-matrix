676dc135f9dcd059b16f19f5dafc6d3bc3233746
jonasbn <jonasbn@gmail.com>
2024-04-26 10:22:18 +0200

    Added support for version 18

--- diags.pl
+++ diags.pl
@@ -11,6 +11,7 @@ use feature qw(say);
 # REF: https://docs.mojolicious.org/Mojo/UserAgent
 
 my %urls = (
+    '18.1.0' => 'https://releases.llvm.org/18.1.0/tools/clang/docs/DiagnosticsReference.html',
     '17.0.1' => 'https://releases.llvm.org/17.0.1/tools/clang/docs/DiagnosticsReference.html',
     '16.0.0' => 'https://releases.llvm.org/16.0.0/tools/clang/docs/DiagnosticsReference.html',
     '15.0.0' => 'https://releases.llvm.org/15.0.0/tools/clang/docs/DiagnosticsReference.html',
